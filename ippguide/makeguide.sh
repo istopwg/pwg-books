e84fd3e31617d970a767aee5dea9a9ec94ab0d0d
Michael R Sweet <michael.r.sweet@gmail.com>
2018-04-26 12:16:25 -0400

    Rename map.md to quickref.md to reflect its content.

--- ippguide/makeguide.sh
+++ ippguide/makeguide.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mmdutil --toc 2 --css ippguide.css intro.md printers.md print-jobs.md map.md >ippguide.html
+mmdutil --toc 2 --css ippguide.css intro.md printers.md print-jobs.md quickref.md >ippguide.html
